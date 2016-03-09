628950bf4f97357e49367bbb5f570257b5f2e498
Matthew Struble <matt@vevox.io>
2016-03-09 15:21:21 -0800

    Grunt stuff

---
+++ Gruntfile.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+  * Aurora MC Gruntfile
+  * http://aurora.vevox.io
+  * @author Matthew Struble
+  */
+module.exports = function (grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    
+
+  });
+
+
+};
